c913dea015a840d4ef1905f2b67e793c3d8b49d3
Jose Geraldo de Carvalho Pereira <jgcarvalho@usp.br>
2016-01-08 15:05:55 -0200

    first functional version

--- versions/v_001_000_000/bin/sga-lncc-buteo_altix-xe_fila/executar.sh
+++ versions/v_001_000_000/bin/sga-lncc-buteo_altix-xe_fila/executar.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+################# valores de teste #####
+# ESSE BLOCO DEVE SER COMENTADO EM PRODUÇÂO
+DIRECTORY="setup"
+DSLSTRING="BEN"
+MINIMIZE="NO"
+NEQUIL=0
+NSTEPS=500
+NITER=100
+RESTRAINTS="harmonic"
+GBSA=""
+NBMETHOD=""
+CUTOFF=1.0
+CONSTRAINTS=""
+TEMPERATURE=298.9
+PRESSURE=1.0
+STORE="store"
 
-. ~/setclasspath.sh
+# . ~/setclasspath.sh
 
 ## Carrega o ambiente de mpi para o Ray
-. /hpc/modulos/bash/openmpi-gcc.sh
+# . /hpc/modulos/bash/openmpi-gcc.sh
 
 function parseArgs {
   for arg in $* ; do
@@ -16,45 +32,87 @@ function parseArgs {
 ######## Parseando argumentos do CSGrid ########
 parseArgs $*
 
-echo $RAY_INPUTTYPES
-echo $numCPU
-echo $RAY_KMERLENGTH
-echo $RAY_INPUT_LEFT_SEQUENCE_FILE
-echo $RAY_INPUT_RIGHT_SEQUENCE_FILE
-echo $RAY_OUTPUT
+echo $DIRECTORY
+echo $DSLSTRING
+echo $STORE
+echo $MINIMIZE
+echo $NEQUIL
+echo $NSTEPS
+echo $NITER
+echo $RESTRAINTS
+echo $GBSA
+echo $NBMETHOD
+echo $CUTOFF
+echo $CONSTRAINTS
+echo $TEMPERATURE
+echo $PRESSURE
+
+#echo $numCPU
 
 cmdId=`echo $cmdId | sed 's/\(.*\)\@\(.*\)\.\(.*\)/\1_\2_\3/'`
 
 echo $cmdId
 
 numCPU=$NSLOTS
+
+
+
 ######## Executando o programa ########
-COMMAND="time mpirun -np $numCPU -hostfile $TMPDIR/hostfile $ALGORITHMS_THIRD_DIR/Ray/versions/v_001_000_000/Ray-2.3.1/Ray"
 
-if [ "$RAY_INPUTTYPES" == "LoadPairedEndReads" ]
-then
-	COMMAND="$COMMAND -k $RAY_KMERLENGTH -p $RAY_INPUT_LEFT_SEQUENCE_FILE $RAY_INPUT_RIGHT_SEQUENCE_FILE -o $RAY_OUTPUT/$cmdId/"
-#        COMMAND="mpirun -np $numCPU $COMMAND -k $KMERLENGTH -p $INPUT_LEFT_SEQUENCE_FILE $INPUT_RIGHT_SEQUENCE_FILE -o $OUTPUT/$cmdId/"
-    echo "Running Ray with LoadPairedEndReads..."
+COMMAND="mkdir $STORE; yank prepare binding amber --setupdir=$DIRECTORY --ligand=\"resname $DSLSTRING\" --store=$STORE  --verbose"
+
+if [[ $MINIMIZE == "TRUE" ]]; then
+  COMMAND="$COMMAND --minimize"
 fi
 
-if [ "$RAY_INPUTTYPES" == "InterleavedSequenceFile" ]
-then
-	COMMAND="$COMMAND -k $RAY_KMERLENGTH -i $RAY_INPUT_INTERLEVED_SEQUENCE_FILE -o $RAY_OUTPUT/$cmdId/"
-    echo "Running Ray with InterleavedSequenceFile..."
+if [ "$NEQUIL" -gt 0 ]; then
+  COMMAND="$COMMAND --equilibrate=$NEQUIL"
 fi
 
-if [ "$RAY_INPUTTYPES" == "LoadSingleEndReads" ]
-then
-	COMMAND="$COMMAND -k $RAY_KMERLENGTH -s $RAY_INPUT_SINGLE_END_SEQUENCE_FILE -o $RAY_OUTPUT/$cmdId/"
-    echo "Running Ray with LoadSingleEndReads..."
+if [ "$NSTEPS" -gt 0 ]; then
+  COMMAND="$COMMAND --nsteps=$NSTEPS"
 fi
+
+if [ "$NITER" -gt 0 ]; then
+  COMMAND="$COMMAND --iterations=$NITER"
+fi
+
+if [[ $RESTRAINTS != "" ]]; then
+  COMMAND="$COMMAND --restraints=$RESTRAINTS"
+fi
+
+if [[ $GBSA != "" ]]; then
+  COMMAND="$COMMAND --gbsa=$GBSA"
+fi
+
+if [[ $NBMETHOD != "" ]]; then
+  COMMAND="$COMMAND --nbmethod=$NBMETHOD"
+fi
+
+if [ "${CUTOFF%.*}" -gt 0 ]; then
+  COMMAND="$COMMAND --cutoff=\"$CUTOFF*nanometer\""
+fi
+
+if [[ $CONSTRAINTS != "" ]]; then
+  COMMAND="$COMMAND --constraints=$CONSTRAINTS"
+fi
+
+if [ "${TEMPERATURE%.*}" -gt 0 ]; then
+  COMMAND="$COMMAND --temperature=\"$TEMPERATURE*kelvin\""
+fi
+
+if [ "${PRESSURE%.*}" -gt 0 ]; then
+  COMMAND="$COMMAND --pressure=\"$PRESSURE*atmosphere\""
+fi
+
+COMMAND="$COMMAND; yank run --store=$STORE --verbose; yank analyze --store=$STORE"
+
 
 echo $COMMAND
-$COMMAND
+eval $COMMAND
 
 if [ $? != 0 ]
 then
-	 echo "Ray returned an error."
+	 echo "Yank returned an error."
          exit 1
 fi
